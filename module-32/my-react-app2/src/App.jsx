2833df4471c8fb69ddf667dc7334b1d9b8c5a908
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-08-20 09:02:13 +0600

    runs

--- module-32/my-react-app2/src/App.jsx
+++ module-32/my-react-app2/src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css'
+import Counter from './Counter';
+import Batsman from './Batsman';
 
 function App() {
 
   function handleClick(){
     alert('I am a click')
   }
+
+  // most of case e eta use korbo
+  const handleClick3 = () =>{
+    alert('Clicked 3')
+  }
+
+  const handleAdd5 = (num) =>{
+    const newNum = num + 5;
+    alert(newNum);
+  }
   return (
     <>
       <h1>Get started Vite + React</h1>
+      <Counter></Counter>
+      <Batsman></Batsman>
       
       <button onClick={handleClick}>Click Me</button>
+      
+      <button onClick={handleClick3}>Click me 3</button>
+
+      <button onClick={() => handleAdd5(10) } > Click me Add 5 </button>
+
     </>
   )
 }
---
+++ module-32/my-react-app2/src/Batsman.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function Batsman(){
+
+    const [runs, setRuns]= useState(0);
+
+    const handleSingle = () => {
+        const updatedRuns = runs + 1;
+        setRuns(updatedRuns)
+    }
+    const handleFour = () => {
+        const updatedRuns = runs + 4;
+        setRuns(updatedRuns)
+    }
+    const handleSix = () => {
+        const updatedRuns = runs + 6;
+        setRuns(updatedRuns)
+    }
+
+    return(
+        <div>
+            <h3>Player: Bangla Batsman</h3>
+            {
+                runs> 50 && <p>Your score: 50</p>
+            }
+            <h1>Score:{runs} </h1>
+            <button onClick={handleSingle}>Single: </button>
+            <button onClick={handleFour}>Four: </button>
+            <button onClick={handleSix}>Six:</button>
+        </div>
+    )
+}
